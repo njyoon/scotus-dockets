09d58981e8787bcbb406929c47f2c6da2d27de6d
Nam Jin <49346772+njyoon@users.noreply.github.com>
2025-01-03 11:42:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,82 +7,6 @@ window.onload = function() {
 
 }
 
-function searchByCite() {
-    
-    document.getElementById('cases').innerHTML = '';
-
-    var volume = document.getElementById('vol').value;
-    var page = document.getElementById('page').value;
-
-    if (volume.length == 0) {
-        return;
-    }
-    
-    let caseList = [];
-
-    for (let element of cases) {
-        
-        if (volume.toString() == element.cite.split(" ")[0]) {
-
-            var volNum = element.cite.split(" ")[0];
-            var pageNum = element.cite.split(" ")[2];
-
-
-            if ((page.length == 0) || (page.toString() == pageNum)) {
-
-                var dkt = "<br>";
-
-                if (element.images) {
-                    dkt += 'Docket Sheet: '
-                    for (let e of element.images) {
-                    dkt += '<a href="'+ e + '" download target="_blank">['+ (element.images.indexOf(e)+1) + ']</a> ';
-                    }
-                } else {
-                    dkt += '<i>Docket Sheet Unavailable</i>';
-                }
-            
-                while (volNum.length < 3) {
-                    volNum = "0" + volNum;
-                }
-    
-                while (pageNum.length < 3) {
-                    pageNum = "0" + pageNum;
-                }
-
-                caseList.push(element.case + '<br>' + element.cite + ' (' + element.year + ')' + ' <a href="https://tile.loc.gov/storage-services/service/ll/usrep/usrep'+ volNum + '/usrep' + volNum + pageNum + '/usrep' + volNum + pageNum + '.pdf" download target="_blank">[Op.]</a>' + dkt);
-
-                if (caseList.length < 4 && element.images) {
-                    var i = '';
-
-
-                    for (let e of element.images) {
-                        i+= '<img src="';
-                        i += e;
-                        i += '">';
-                    }
-
-                    caseList.push(i);
-                    
-                }
-            }
-        }
-
-        if (caseList.length > 99) {
-            break;
-        }
-    }
-
-
-    caseList.forEach((e)=> {
-        document.getElementById('cases').innerHTML += '<p>' + e + '</p>';
-    });
-
-    if (caseList.length > 99) {
-        document.getElementById('cases').innerHTML += '<p>...</p>';
-    }
-
-}
-
 function updateSearchResults(keyword) {
 
     document.getElementById('cases').innerHTML = '';
@@ -139,7 +63,7 @@ function updateSearchResults(keyword) {
                             for (let e of element.images) {
                                 i+= '<img src="';
                                 i += e;
-                                i += '">';
+                                i += '" loading="lazy">';
                             }
 
                             caseList.push(i);
@@ -150,7 +74,7 @@ function updateSearchResults(keyword) {
             }
         }
 
-        if (caseList.length > 99) {
+        if (caseList.length > 49) {
             break;
         }
     }
